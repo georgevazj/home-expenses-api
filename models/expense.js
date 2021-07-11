2051e31cc71312f7e88b052bc608e06b1fd84013
Jorge <georgevazj@gmail.com>
2021-07-11 23:45:57 +0200

    Modelo para los gastos

---
+++ models/expense.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+
+const ExpenseSchema = Schema({
+    fecha: {
+        type: Date,
+        required: [true, 'La fecha es obligatoria']
+    },
+    categoria: {
+        type: String,
+        required: [true, 'La categoria es obligatoria']
+    },
+    subcategoria: {
+        type: String,
+        default: 'Compras (otros)'
+    },
+    descripcion: {
+        type: String,
+        required: [true, 'La descripcion es obligatoria']
+    },
+    comentario: {
+        type: String,
+    },
+    importe: {
+        type: Number,
+        required: [true, 'El importe es obligatorio']
+    },
+})
+
+ExpenseSchema.methods.toJson = function() {
+    const { __v, ...expense } = this.toObject()
+    return expense
+}
+
+module.exports = model('Expense', ExpenseSchema)
